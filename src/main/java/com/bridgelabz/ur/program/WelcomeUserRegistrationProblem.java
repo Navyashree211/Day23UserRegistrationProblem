c5e3d2008f9afadabc37089c4b50ee5345ea03f4
navyas211@gmail.com <navyas211@gmail.com>
2021-10-21 01:24:31 +0530

    [ADDED]: welcome message

---
+++ src/main/java/com/bridgelabz/ur/program/WelcomeUserRegistrationProblem.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.ur.program;
+
+public class WelcomeUserRegistrationProblem {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration Problem By Using Java Regular Expression");
+	}
+}
