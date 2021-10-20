f214bdd747bb1481a984e1a2345377d527cf84b0
navyas211@gmail.com <navyas211@gmail.com>
2021-10-21 01:31:22 +0530

    [ADDED]: enter a valid last name

--- src/main/java/com/bridgelabz/ur/program/UserRegistration.java
+++ src/main/java/com/bridgelabz/ur/program/UserRegistration.java
@@ -1,5 +1,5 @@
 /**
- * Valid First Name with first letter starting with capital and has minimum 3 characters in it.
+ * Valid Last Name with first letter starting with capital and has minimum 3 characters in it.
  *
  * @author: Navya Shree
  * @since: 20.10.2021
@@ -10,13 +10,13 @@ package com.bridgelabz.ur.program;
 public class UserRegistration {
 
 	public static void main(String[] args) {
-		System.out.println(firstName("Navya"));
-		System.out.println(firstName("Anokhi"));
+		System.out.println(lastName("Shree"));
+		System.out.println(lastName("Sha"));
 	}
 
-	// validate first name
-	public static boolean firstName(String FirstName) {
-		return FirstName.matches("^[A-Z]{1}[a-z]{2,}$");
-
+	// validate last name
+	public static boolean lastName(String lastName) {
+		return lastName.matches("^[A-Z]{1}[a-z]{2,}$");
 	}
+
 }
