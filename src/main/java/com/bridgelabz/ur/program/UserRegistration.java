be89854a303a841d4ae5728491e0a9a621d80c82
navyas211@gmail.com <navyas211@gmail.com>
2021-10-21 01:36:31 +0530

    [ADDED]: pre-defined mobile format

--- src/main/java/com/bridgelabz/ur/program/UserRegistration.java
+++ src/main/java/com/bridgelabz/ur/program/UserRegistration.java
@@ -1,7 +1,7 @@
 /**
- * check valid Email
- * EX:- ac.xyz@bl.co.in
- * Email has 3 mandatory parts (abc,bl & co) annd 2 optional (xyz & in) with precise @ and . positions .
+  * User need to follow pre-defiend Mobile Format .
+ * Ex:-91 9919819801
+ * country code follow by space and 10 digit number .
  * 
  * @author: Navya Shree
  * @since: 20.10.2021
@@ -13,19 +13,18 @@ public class UserRegistration {
 
 	public static void main(String[] args) {
 
-		email();
+		phoneNumber();
 	}
 
-	// validate given email
-	public static void email() {
-		String email = "abc.xyz@bl.co.in";
-		String regex = "^[a-z]{3}[a-zA-Z0-9+_.-]*@[a-z]{2}[.]{1}[a-z]{2}[.][a-z]*$";
-		boolean result = email.matches(regex);
+	// validate given phone number
+	public static void phoneNumber() {
+		String phone = "91  9919819801";
+		String regex = "[0-9]{2}[%s][0-9]{10}";
+		boolean result = phone.matches(regex);
 		if (result) {
-			System.out.println("Given email-id is valid");
+			System.out.println("Given phone number is valid");
 		} else {
-			System.out.println("Given email-id is not valid");
+			System.out.println("Given phone number is not valid ");
 		}
-
 	}
 }
