1108fc7b860980e150869646c882e04e3da19ea4
navyas211@gmail.com <navyas211@gmail.com>
2021-10-21 01:34:03 +0530

    [ADDED]: enter a valid email

--- src/main/java/com/bridgelabz/ur/program/UserRegistration.java
+++ src/main/java/com/bridgelabz/ur/program/UserRegistration.java
@@ -1,6 +1,8 @@
 /**
- * Valid Last Name with first letter starting with capital and has minimum 3 characters in it.
- *
+ * check valid Email
+ * EX:- ac.xyz@bl.co.in
+ * Email has 3 mandatory parts (abc,bl & co) annd 2 optional (xyz & in) with precise @ and . positions .
+ * 
  * @author: Navya Shree
  * @since: 20.10.2021
  */
@@ -10,13 +12,20 @@ package com.bridgelabz.ur.program;
 public class UserRegistration {
 
 	public static void main(String[] args) {
-		System.out.println(lastName("Shree"));
-		System.out.println(lastName("Sha"));
+
+		email();
 	}
 
-	// validate last name
-	public static boolean lastName(String lastName) {
-		return lastName.matches("^[A-Z]{1}[a-z]{2,}$");
-	}
+	// validate given email
+	public static void email() {
+		String email = "abc.xyz@bl.co.in";
+		String regex = "^[a-z]{3}[a-zA-Z0-9+_.-]*@[a-z]{2}[.]{1}[a-z]{2}[.][a-z]*$";
+		boolean result = email.matches(regex);
+		if (result) {
+			System.out.println("Given email-id is valid");
+		} else {
+			System.out.println("Given email-id is not valid");
+		}
 
+	}
 }
